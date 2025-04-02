d49b53d823543af947aa659a25ca6b2497523237
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-04-02 10:34:11 +0200

    js-assignment-5-1
    created the menu in the console with the different options

---
+++ main.js
@@ -0,0 +1,10 @@
+function atm () {
+    while (true) {
+        let choice = parseInt(
+            prompt(
+                "Select a choice:\n1.) See balance\n2.) Make a deposit\n3.) Make a withdrawal\n4.) Get account name\n5.) Exit"
+            )
+        )
+    }
+}
+atm()
