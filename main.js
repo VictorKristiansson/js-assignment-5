199543e0f3573201d86d3f21b3363f902c4b2bfa
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-04-03 09:48:39 +0200

    js-assignment-5
    Started working on the project. 3 of the 5 functions are done (case 1, 4, 5).

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-function atm () {
-    while (true) {
-        let choice = parseInt(
-            prompt(
-                "Select a choice:\n1.) See balance\n2.) Make a deposit\n3.) Make a withdrawal\n4.) Get account name\n5.) Exit"
-            )
+
+//ACCOUNT
+const account = {
+    balance : 1000,
+    accountName : "Pablo Escobar",
+
+    getBalance: function() {
+        alert(`Your balance is: $${this.balance}`);
+    },
+
+    getName: function() {
+        alert(`Account name: ${this.accountName}`);
+    }
+};
+
+//THE ATM FUNCTION
+function atm() {
+    const message = parseFloat(
+        prompt(
+            "Select a choice!\n 1.) See balance\n 2.) Make a deposit\n 3.) Make a withdrawal\n 4.) Get account name\n 5.) Exit\n"
         )
+    );
+
+    switch (message) {
+        
+        case 1: 
+            account.getBalance();
+            break;
+        
+        case 2: 
+        
+        case 3:
+
+        case 4:
+            account.getName();
+            break;
+        
+        case 5: 
+            break;
+
     }
 }
+
 atm()
